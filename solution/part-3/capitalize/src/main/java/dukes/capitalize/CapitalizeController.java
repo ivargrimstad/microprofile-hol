80c60d6c9c88d195e031e8312303f75cf5548984
Phillip Kruger <phillip.kruger@gmail.com>
2019-07-23 14:22:37 +0200

    Added doc for capitalized server

--- solution/part-3/capitalize/src/main/java/dukes/capitalize/CapitalizeController.java
+++ solution/part-3/capitalize/src/main/java/dukes/capitalize/CapitalizeController.java
@@ -4,9 +4,11 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import org.eclipse.microprofile.opentracing.Traced;
 
 @Path("/capitalize")
 @ApplicationScoped
+@Traced
 public class CapitalizeController {
 
     @GET
