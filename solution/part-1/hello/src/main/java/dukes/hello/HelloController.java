1db60e5da5a07e9fff5bc11af605c6d4436cc95a
Ivar Grimstad <ivar.grimstad@gmail.com>
2019-07-24 10:37:53 +0200

    Fix typo

--- solution/part-1/hello/src/main/java/dukes/hello/HelloController.java
+++ solution/part-1/hello/src/main/java/dukes/hello/HelloController.java
@@ -3,6 +3,9 @@ package dukes.hello;
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 
 /**
  *
@@ -12,6 +15,7 @@ import javax.ws.rs.Path;
 public class HelloController {
 
     @GET
+    @Produces(TEXT_PLAIN)
     public String sayHello() {
         return "Hello World";
     }
